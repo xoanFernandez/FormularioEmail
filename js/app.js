7490377fab63ca27a888ec6109cc4cd131d4da17
xoanFernandez <xoanfernandez94@gmail.com>
2023-05-27 17:44:48 +0200

    finalizado

--- js/app.js
+++ js/app.js
@@ -13,11 +13,51 @@ document.addEventListener('DOMContentLoaded', function () {
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner')
 
     //asiganar eventos
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
+    formulario.addEventListener('submit', enviarEmail);
+
+    btnReset.addEventListener('click', function (e) {
+
+        e.preventDefault();
+
+       resetFormulario();
+
+    })
+
+    function enviarEmail(e) {
+        e.preventDefault();
+
+        spinner.classList.add('flex')
+        spinner.classList.remove('hidden');
+
+        setTimeout(() => {
+
+            spinner.classList.remove('flex')
+            spinner.classList.add('hidden');
+
+            resetFormulario();
+
+            const alertaExito = document.createElement('P');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 
+            'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent = 'Mensaje enviado correctamente';
+
+            formulario.appendChild(alertaExito);
+
+            setTimeout(() => {
+                alertaExito.remove();
+            }, 3000);
+
+        }, 3000);
+
+
+    }
 
     function validar(e) {
 
@@ -80,6 +120,17 @@ document.addEventListener('DOMContentLoaded', function () {
       
         btnSubmit.classList.remove('opacity-50');
         btnSubmit.disabled = false;
+    }
+
+    function resetFormulario() {
+
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+
+        formulario.reset();
+        comprobarEmail();
+
     }
 
 });
